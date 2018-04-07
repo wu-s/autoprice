c938475020c3f93f61493926aeba1ce3efdd5a83
wu-s <wu-s@klab.com>
2018-04-07 20:23:49 +0800

    add mail feature

--- data/schema.sql
+++ data/schema.sql
@@ -1,18 +1,18 @@
-
 CREATE TABLE `inquiry_record` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `inquiry_time` datetime NOT NULL,
   `state` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
   `zip` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `code` int(11) DEFAULT NULL,
   `utility_provider` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `price` decimal(12,2) not null default 0,
-  `type` int(11) NOT NULL default 1,
+  `price` decimal(12,2) NOT NULL DEFAULT '0.00',
+  `type` int(11) NOT NULL DEFAULT '1',
   `insert_date` datetime NOT NULL,
   `update_date` datetime NOT NULL,
-  `url` text,
-  `params` text,
-  `response` text,
+  `url` text COLLATE utf8_unicode_ci,
+  `params` text COLLATE utf8_unicode_ci,
+  `response` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   KEY `inquiry_time` (`inquiry_time`,`state`),
   KEY `state` (`state`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5138 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
